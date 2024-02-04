e89cfd8e107fac92d493ab014adbdd8471ba98f6
Lekhchine Ouail <114731267+bridzy@users.noreply.github.com>
2024-02-04 14:41:50 +0100

    Update App.java

--- src/main/java/com/fges/todoapp/App.java
+++ src/main/java/com/fges/todoapp/App.java
@@ -1,16 +1,15 @@
 package com.fges.todoapp;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-import com.fasterxml.jackson.databind.node.MissingNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.commons.cli.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class App {
@@ -35,17 +34,22 @@ class CommandLineHandler {
     public Command parse(String[] args) {
         Options options = new Options();
         options.addRequiredOption("s", "source", true, "File containing the todos");
+        options.addOption("d", "done", false, "Mark todo as done");
+        
         CommandLineParser parser = new DefaultParser();
         try {
             CommandLine cmd = parser.parse(options, args);
             String fileName = cmd.getOptionValue("s");
+            boolean isDone = cmd.hasOption("d");
             List<String> positionalArgs = cmd.getArgList();
+            
             if (positionalArgs.isEmpty()) {
                 System.err.println("Missing Command");
                 return null;
             }
             String commandType = positionalArgs.get(0);
-            return new Command(fileName, commandType, positionalArgs);
+            
+            return new Command(fileName, commandType, positionalArgs, isDone);
         } catch (ParseException ex) {
             System.err.println("Fail to parse arguments: " + ex.getMessage());
             return null;
@@ -57,11 +61,13 @@ class Command {
     private String fileName;
     private String commandType;
     private List<String> arguments;
+    private boolean isDone;
 
-    public Command(String fileName, String commandType, List<String> arguments) {
+    public Command(String fileName, String commandType, List<String> arguments, boolean isDone) {
         this.fileName = fileName;
         this.commandType = commandType;
         this.arguments = arguments;
+        this.isDone = isDone;
     }
 
     public String getFileName() {
@@ -75,60 +81,47 @@ class Command {
     public List<String> getArguments() {
         return arguments;
     }
+
+    public boolean isDone() {
+        return isDone;
+    }
 }
 
 interface FileFormatHandler {
-    String readContent(String fileName) throws IOException;
-    void writeContent(String fileName, String content) throws IOException;
-    void processInsertCommand(String todo);
-    void processListCommand();
+    void processInsertCommand(String fileName, String todo, boolean isDone) throws IOException;
+    void processListCommand(String fileName, boolean onlyDone) throws IOException;
 }
 
 class JsonFormatHandler implements FileFormatHandler {
-    private String content;
+    private ObjectMapper mapper = new ObjectMapper();
 
-    public String readContent(String fileName) throws IOException {
+    public void processInsertCommand(String fileName, String todo, boolean isDone) throws IOException {
+        Path path = Paths.get(fileName);
+        List<ObjectNode> todos = new ArrayList<>();
+        if (Files.exists(path)) {
+            String content = Files.readString(path);
+            todos = mapper.readValue(content, new TypeReference<List<ObjectNode>>(){});
+        }
+        
+        ObjectNode newTodo = mapper.createObjectNode();
+        newTodo.put("task", todo);
+        newTodo.put("done", isDone);
+        todos.add(newTodo);
+        
+        Files.writeString(path, mapper.writeValueAsString(todos));
+    }
+
+    public void processListCommand(String fileName, boolean onlyDone) throws IOException {
         Path path = Paths.get(fileName);
         if (Files.exists(path)) {
-            content = Files.readString(path);
-        } else {
-            content = "";
-        }
-        return content;
-    }
-
-    public void writeContent(String fileName, String content) throws IOException {
-        Files.writeString(Paths.get(fileName), content);
-    }
-
-    public void processInsertCommand(String todo) {
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            JsonNode actualObj = mapper.readTree(content);
-            if (actualObj instanceof MissingNode || !actualObj.isArray()) {
-                actualObj = JsonNodeFactory.instance.arrayNode();
-            }
-
-            ((ArrayNode) actualObj).add(todo);
-            content = actualObj.toString();
-        } catch (IOException e) {
-            content = JsonNodeFactory.instance.arrayNode().toString();
-        }
-    }
-
-    public void processListCommand() {
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            JsonNode actualObj = mapper.readTree(content);
-            if (actualObj instanceof ArrayNode) {
-                actualObj.forEach(node -> System.out.println("- " + node.asText()));
-            }
-        } catch (IOException e) {
-            System.err.println("Error reading JSON content");
+            String content = Files.readString(path);
+            List<ObjectNode> todos = mapper.readValue(content, new TypeReference<List<ObjectNode>>(){});
+            todos.stream()
+                .filter(todo -> !onlyDone || todo.get("done").asBoolean())
+                .forEach(todo -> System.out.println((todo.get("done").asBoolean() ? "Done: " : "") + todo.get("task").asText()));
         }
     }
 }
-
 class CsvFormatHandler implements FileFormatHandler {
     private String content;
 
@@ -164,14 +157,10 @@ class TodoProcessor {
     public int processCommand(Command command) throws IOException {
         if (command.getFileName().endsWith(".json")) {
             formatHandler = new JsonFormatHandler();
-        } else if (command.getFileName().endsWith(".csv")) {
-            formatHandler = new CsvFormatHandler();
         } else {
             System.err.println("Unsupported file format");
             return 1;
         }
-
-        String fileContent = formatHandler.readContent(command.getFileName());
 
         if (command.getCommandType().equals("insert")) {
             if (command.getArguments().size() < 2) {
@@ -179,15 +168,13 @@ class TodoProcessor {
                 return 1;
             }
             String todo = command.getArguments().get(1);
-            formatHandler.processInsertCommand(todo);
+            formatHandler.processInsertCommand(command.getFileName(), todo, command.isDone());
         } else if (command.getCommandType().equals("list")) {
-            formatHandler.processListCommand();
+            formatHandler.processListCommand(command.getFileName(), command.isDone());
         } else {
             System.err.println("Invalid Command");
             return 1;
         }
-
-        formatHandler.writeContent(command.getFileName(), fileContent);
         return 0;
     }
 }
